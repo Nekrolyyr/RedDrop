0af8a85032767adb05eab794d599d04095cada3f
Jaco Fuchs <jaco.fuchs@gmail.com>
2020-04-04 13:40:55 +0200

    lets try2

--- app/src/main/java/ch/versusvirus/reddrop/ui/HomeActivity.java
+++ app/src/main/java/ch/versusvirus/reddrop/ui/HomeActivity.java
@@ -2,6 +2,7 @@ package ch.versusvirus.reddrop.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         findViewById(R.id.btn_donate).setOnClickListener(v -> {
             startActivity(new Intent(this, SurveyResultActivity.class));
         });
--- app/src/main/java/ch/versusvirus/reddrop/ui/SurveyResultActivity.java
+++ app/src/main/java/ch/versusvirus/reddrop/ui/SurveyResultActivity.java
@@ -10,44 +10,24 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
-
 import ch.versusvirus.reddrop.R;
-import ch.versusvirus.reddrop.logic.model.Questions;
 
 public class SurveyResultActivity extends AppCompatActivity {
 
-    Button btn_donate, btn_redo, btn_home;
-
-    TextView text_field_question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_result);
-        /*findViewById(R.id.btn_submit).setOnClickListener(v -> {
-            startActivity(new Intent(this, LocationActivity.class));
-        });*/
-
-        btn_donate = (Button) findViewById(R.id.btn_donate);
-        btn_redo = (Button) findViewById(R.id.btn_redo);
-        btn_home = (Button) findViewById(R.id.btn_home);
-
-        text_field_question = (TextView) findViewById(R.id.text_field_question);
-
-
-        btn_donate.setOnClickListener(v -> {
-            startActivity(new Intent(this, LocationActivity.class));
-        });
-
-        btn_redo.setOnClickListener(v -> {
-            startActivity(new Intent(this,SurveyActivity.class));
-        });
-
-        btn_donate.setOnClickListener(v -> {
+        findViewById(R.id.btn_donate).setOnClickListener(v -> {
             startActivity(new Intent(this, SurveyActivity.class));
+        });
+        findViewById(R.id.btn_redo).setOnClickListener(v -> {
+            startActivity(new Intent(this, ProfileActivity.class));
+        });
+        findViewById(R.id.btn_home).setOnClickListener(v -> {
+            startActivity(new Intent(this, HomeActivity.class));
         });
 
     }
-
 }
