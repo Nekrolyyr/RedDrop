f96ad31177741f65819e8d7d15defce8b9c88c27
luca <luca.pedrelli1@gmail.com>
2020-04-04 14:41:18 +0200

    Merge remote-tracking branch 'origin/myProfile' into myProfile
    # Conflicts:
    #	app/src/main/res/layout/activity_profile.xml

--- app/src/main/java/ch/versusvirus/reddrop/ui/ProfileActivity.java
+++ app/src/main/java/ch/versusvirus/reddrop/ui/ProfileActivity.java
@@ -1,19 +1,87 @@
 package ch.versusvirus.reddrop.ui;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.CollationElementIterator;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import ch.versusvirus.reddrop.R;
 
+
 public class ProfileActivity extends AppCompatActivity {
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.male:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.female:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        final Calendar myCalendar = Calendar.getInstance();
 
+        EditText edittext= (EditText) findViewById(R.id.dateOfBirth);
+        
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+                DatePickerDialog dialog = new DatePickerDialog(v.getContext(), date,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+        });
 
     }
-}
+
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        CollationElementIterator edittext = null;
+        Calendar myCalendar = null;
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+};
