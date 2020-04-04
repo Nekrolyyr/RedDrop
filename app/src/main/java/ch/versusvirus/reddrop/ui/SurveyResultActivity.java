e4528f76dbdd17f84e8b147b94d7e2d0c2af6a6a
Jaco Fuchs <jaco.fuchs@gmail.com>
2020-04-04 14:06:06 +0200

    lets try2

--- app/src/main/java/ch/versusvirus/reddrop/ui/SurveyResultActivity.java
+++ app/src/main/java/ch/versusvirus/reddrop/ui/SurveyResultActivity.java
@@ -1,32 +1,28 @@
 package ch.versusvirus.reddrop.ui;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ch.versusvirus.reddrop.R;
 
 public class SurveyResultActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_result);
-        findViewById(R.id.btn_donate).setOnClickListener(v -> {
-            startActivity(new Intent(this, SurveyActivity.class));
+
+        findViewById(R.id.btn_donate_now).setOnClickListener(v -> {
+            startActivity(new Intent(this, LocationActivity.class));
         });
         findViewById(R.id.btn_redo).setOnClickListener(v -> {
             startActivity(new Intent(this, ProfileActivity.class));
         });
-        findViewById(R.id.btn_home).setOnClickListener(v -> {
-            startActivity(new Intent(this, HomeActivity.class));
+        findViewById(R.id.home).setOnClickListener(v -> {
+            //TODO: Add donation history / favourites
         });
 
     }
